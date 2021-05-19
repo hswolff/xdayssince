5783eca0e64ee77f14b17e885566484e57b44c1b
Harry Wolff <hswolff@gmail.com>
2021-05-18 21:19:24 -0400

    Add support for a user to login with NextAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,39 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Provider } from 'next-auth/client';
 import Link from 'next/link';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/profile">
-              <a>Profile</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/create">
-              <a>Create</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/i/bananas">
-              <a>Banana Incident</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Component {...pageProps} />
-    </div>
+    <Provider session={pageProps.session}>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/profile">
+                <a>Profile</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/create">
+                <a>Create</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/i/bananas">
+                <a>Banana Incident</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Component {...pageProps} />
+      </div>
+    </Provider>
   );
 }
 
--- pages/create.tsx
+++ pages/create.tsx
@@ -1,13 +1,28 @@
 import Head from 'next/head';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from '../styles/Home.module.css';
 
 export default function CreatePage() {
+  const [session, loading] = useSession();
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create | XDaysSince</title>
       </Head>
 
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session?.user?.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
       <main className={styles.main}>
         <h1 className={styles.title}>Create a new XDaysSince</h1>
       </main>
